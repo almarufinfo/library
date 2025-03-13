81034dacabf2b4f717c3e7817d5787695652239b
alMaruf <almarufinfo@gmail.com>
2025-03-13 16:56:01 +0600

    api added

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party rest frammwork
+    "rest_framework",
+
     # local
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
+
+    
 ]
 
 MIDDLEWARE = [
